40421364c868c70d918a1405fa9f5ee9bc3db756
Willylyd <willylyd@student.21-school.ru>
2024-05-02 22:08:21 +0700

    check_positions

--- src/main/java/ru/fev/accumulation/entity/CheckPosition.java
+++ src/main/java/ru/fev/accumulation/entity/CheckPosition.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "check_position")
+@Table(name = "check_positions")
 public class CheckPosition {
 
     @Id
