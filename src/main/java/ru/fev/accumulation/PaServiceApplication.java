a179981c1fd1ddc5aa79b87103b477476ce2210c
willylyd <a18756@gmail.com>
2024-07-08 18:11:13 +0700

    CORS

--- src/main/java/ru/fev/accumulation/PaServiceApplication.java
+++ src/main/java/ru/fev/accumulation/PaServiceApplication.java
@@ -2,12 +2,25 @@ package ru.fev.accumulation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PaServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/").allowedOrigins("http://localhost:9966");
+            }
+        };
     }
 
 }
