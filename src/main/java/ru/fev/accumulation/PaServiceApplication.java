4d425790f9810c94c32b04066759493394543acd
willylyd <a18756@gmail.com>
2024-08-05 19:37:02 +0700

    edit
    Please enter the commit message for your changes. Lines starting

--- src/main/java/ru/fev/accumulation/PaServiceApplication.java
+++ src/main/java/ru/fev/accumulation/PaServiceApplication.java
@@ -13,14 +13,4 @@ public class PaServiceApplication {
         SpringApplication.run(PaServiceApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/").allowedOrigins("http://localhost:9966");
-            }
-        };
-    }
-
 }
--- src/main/java/ru/fev/accumulation/service/CheckPositionsServiceImpl.java
+++ src/main/java/ru/fev/accumulation/service/CheckPositionsServiceImpl.java
@@ -2,6 +2,7 @@ package ru.fev.accumulation.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.fev.accumulation.entity.CheckPosition;
 import ru.fev.accumulation.exceptions.PAEntityNotFoundException;
